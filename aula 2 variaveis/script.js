84cad24589f2e2d3a9d35e8233eaca6de6e7339b
Pierre Souza <pas@lionx.com.br>
2022-07-15 17:54:13 -0300

    update

---
+++ aula 2 variaveis/script.js
@@ -0,0 +1,77 @@
+//tipos primitivos
+
+//boolean
+// var vOuF = false;
+// console.log(typeof vOuF);
+
+//number
+// var numeroQualquer = 1;
+// console.log(typeof numeroQualquer);
+
+//string
+// var nome = "diana";
+// console.log(typeof nome);
+
+//function
+// var funcao = function () {};
+// console.log(typeof funcao);
+
+// como declarar
+// var variavel = "diana";
+// variavel = "martine";
+// console.log(variavel);
+
+// let variavel2 = "diana";
+// variavel2 = "martine";
+// console.log(variavel2);
+
+// const constante = "diana";
+// constante = "martine";
+// console.log(constante);
+
+var escopoGlobal = "global";
+// console.log(escopoGlobal);
+
+// function escopoLocal() {
+//   let escopoLocalInterno = "local";
+//   console.log(escopoLocalInterno);
+// }
+
+// escopoLocal();
+
+//atribuição
+// var atribuicao = "diana";
+
+// //comparação
+
+// var comparacao = "0" == 0;
+// console.log(comparacao);
+
+// //comparação identica
+// var comparacaoIdentica = "0" === 0;
+// console.log(comparacaoIdentica);
+
+// operadores aritmétcos
+//adição
+var adição = 1 + 1;
+console.log(adição);
+
+//subtração
+var subtracao = 2 - 1;
+console.log(subtracao);
+
+//multiplicação
+var multiplicacao = 2 * 3;
+console.log(multiplicacao);
+
+//divisão real
+var divisaoReal = 6 / 2;
+console.log(divisaoReal);
+
+//divisão inteira
+var divisaoInteira = 5 % 2;
+console.log(divisaoInteira);
+
+//potenciação
+var potenciacao = 2 ** 10;
+console.log(potenciacao);
